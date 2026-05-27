91d3fea9c905ea013f10723f737c5344be9dc60b
AstroTati <tatimr93@gmail.com>
2026-05-27 11:30:22 +0000

    Completed silver layer data transformations

--- scripts/silver/silver_data_transformations.py
+++ scripts/silver/silver_data_transformations.py
@@ -11,12 +11,12 @@ _______________________________________________________________________________
 from datetime import datetime
 
 notebooks = [
-    "./crm_cust_info_transformandload",
-    "./crm_prd_info_transformandload",
-    "./silver_crm_sales_details",
-    "./silver_erp_cust_az12",
-    "./silver_erp_loc_a101",
-    "./silver_erp_px_cat_g1v2"
+    "./crm_cust_info_transformandload.ipynb",
+    "./crm_prd_info_transformandload.ipynb",
+    "./crm_sales_details_transformandload.ipynb",
+    "./erp_cust_az12_transformandload.ipynb",
+    "./erp_loc_a101_transformandload.ipynb",
+    "./erp_px_cat_g1v2_transformandload.ipynb"
 ]
 
 print(f">>> [{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}]: BEGIN DATA TRANSFORMATION AND LOAD INTO SILVER LAYER")
